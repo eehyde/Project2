3dd14e53e55ff610f38cd9b011d0f0479eeb9f8a
Emily Hyde <eehyde@umich.edu>
2017-02-04 18:48:52 -0500

    Adding answers to part 2 and the computer_paths.txt file

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -49,18 +49,35 @@ def parse_counted_words(astring):
 
 ## PART 2: 200 points
 
-## We have provided a text file computer_paths.txt. It's not incredibly long -- you can scan through it, but do NOT hard code your answers! Each line contains 1 filesystem path.
+## We have provided a text file computer_paths.txt. It's not incredibly long -- you can 
+#scan through it, but do NOT hard code your answers! Each line contains 1 filesystem path.
+afile = open("computer_paths.txt","r")
+w = afile.read()
+## (a) Write Python code to determine how many of these paths identify FILES, not 
+#directories. Save that number in the variable file_paths_num.
 
-## (a) Write Python code to determine how many of these paths identify FILES, not directories. Save that number in the variable file_paths_num.
-
-## (b) Write Python code to determine how many of these paths are FULL paths, not relative paths. Save that number in the variable full_paths_num.
-
-## (c) Write Python code to determine how many of these paths describe a Python file saved inside a folder called SI206. Save that number in the variable python_course_paths.
-
-## (d) Write Python code to determine how many of these paths describe a Microsoft file (a file that EITHER ends with .docx OR .xlsx, but nothing else counts) where the file name ends in a digit. Save that total in the variable microsoft_files_num.
+files = re.findall(r'\w+[.]',w)
+file_paths_num = len(files)
 
 
+## (b) Write Python code to determine how many of these paths are FULL paths, not relative 
+#paths. Save that number in the variable full_paths_num.
 
+fullpath = re.findall(r'\/Users\/|~\/',w)
+full_paths_num = len(fullpath)
+
+## (c) Write Python code to determine how many of these paths describe a Python file saved 
+#inside a folder called SI206. Save that number in the variable python_course_paths.
+
+python = re.findall(r'SI206\/.+(.py)+',w)
+python_course_paths = len(python)
+
+## (d) Write Python code to determine how many of these paths describe a Microsoft file 
+#(a file that EITHER ends with .docx OR .xlsx, but nothing else counts) where the file 
+#name ends in a digit. Save that total in the variable microsoft_files_num.
+
+word = re.findall(r'[0-9]\.docx|[0-9]\.xlsx',w)
+microsoft_files_num = len(word)
 
 
 
